3a123611bee462c3e5a286cddbadeaf6a4470126
Toby Winter <toby.hw@live.co.uk>
2017-10-27 18:37:52 +0100

    Adds test for call for users, Adds :users method

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -7,4 +7,8 @@ class ApiClient
     response = HTTParty.get("https://driftrock-dev-test-2.herokuapp.com/status")
     response.code
   end
+
+  def users
+    response = HTTParty.get("https://driftrock-dev-test-2.herokuapp.com/users?page=1&per_page=20")
+  end
 end
--- spec/api_client_spec.rb
+++ spec/api_client_spec.rb
@@ -1,8 +1,12 @@
 describe ApiClient do
   subject(:api_client) { described_class.new }
 
-  it 'calls API with :checkstatus' do
-    expect(api_client.checkstatus).to eq 200
+  it 'calls API with :check_status' do
+    expect(api_client.check_status).to eq 200
+  end
+
+  it 'queries api for users with :users' do
+    expect(api_client.users).to include("data")
   end
 
 end
