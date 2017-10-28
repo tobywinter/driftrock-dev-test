55963de0f73b8289b1017b72f2d488dbd4b84ba0
Toby Winter <toby.hw@live.co.uk>
2017-10-28 22:43:33 +0100

    can search for user by email

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -5,22 +5,30 @@ class ApiClient
   BASE_URI = "https://driftrock-dev-test-2.herokuapp.com"
   PAGE_LENGTH = 2000
 
+  attr_reader :users
+
+  def initialize
+    @users = []
+  end
+
   def check_status
     response = HTTParty.get("#{BASE_URI}/status")
     response.code
   end
 
-  def users
+  def get_users
     i = 1
     data_available = true
-    users = []
     while data_available do
       response = HTTParty.get("#{BASE_URI}/users?page=#{i}&per_page=#{PAGE_LENGTH}")
-      users += JSON.parse(response.body)["data"]
+      @users += JSON.parse(response.body)["data"]
       data_available = page_full?(response)
       i += 1
     end
-    users
+  end
+
+  def find_user(email)
+    @users.find {|user| user["email"] == email }
   end
 
 private
--- spec/api_client_spec.rb
+++ spec/api_client_spec.rb
@@ -1,12 +1,12 @@
 describe ApiClient do
   subject(:api_client) { described_class.new }
 
-  it 'calls API with :check_status' do
-    expect(api_client.check_status).to eq 200
+  before do
+    api_client.get_users
   end
 
-  it 'queries api for users with :users' do
-    expect(api_client.users[0]).to include("id")
+  it 'calls API with :check_status' do
+    expect(api_client.check_status).to eq 200
   end
 
   context 'Users' do
@@ -15,9 +15,18 @@ describe ApiClient do
       expect(api_client.users.class).to eq Array
     end
 
-    # it 'can search for a user by email' do
-    #   expect(api_client.users)
-    # end
+    it 'get_users saves user data in @users' do
+      expect(api_client.users[0]).to include("id")
+    end
+
+    it 'can search for a user by email' do
+      user = {"id"=>"MQIK-25DG-IG10-EA5I",
+         "first_name"=>"Damaris",
+         "last_name"=>"Strosin",
+         "phone"=>"1-612-206-7708",
+         "email"=>"damaris.strosin@ruel.name"}
+      expect(api_client.find_user('damaris.strosin@ruel.name')).to eq user
+    end
 
   end
 
